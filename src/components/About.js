5fc28d2487615833d7c3bc996ffc70b0ea090581
Matthew Rigdon <rigdonmr@gmail.com>
2018-06-28 13:26:50 -0700

    Got layout correct

--- src/components/About.js
+++ src/components/About.js
@@ -1,13 +1,22 @@
 import React from 'react';
-import me from '../images/me.png';
+import SeeMore from './SeeMore';
 
-const imgWidth = 150;
+const bio = 'Through years of professional experience & a deep passion for building great user experiences, Matt has developed expertise in HTML, CSS, JavaScript, and Ruby -- 4 fundamental building blocks for developing modern, responsive webapps.';
 
 export default function About() {
 
   return (
     <main className="about">
-      <div className="me-img" />
+      <div className="container about__page-1">
+        <div className="about__page-1__bio">
+          <div className="me-img" />
+          <h1>Matt Rigdon</h1>
+          <h3 className="text--secondary">Software Developer at Salesforce</h3>
+          <p>{bio}</p>
+        </div>
+
+        <SeeMore />
+      </div>
     </main>
   );
 }
---
+++ src/components/SeeMore.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function SeeMore() {
+  return (
+    <div className="see-more">
+      <i className="see-more__arrow" />
+    </div>
+  );
+}
